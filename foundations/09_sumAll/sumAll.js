593e7182de5e3ad808af37791ac779eb838eb2f7
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-14 19:59:30 -0500

    finished foundations09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) return 'ERROR';
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+  let smaller = x < y ? x : y;
+  let larger = x > y ? x : y;
+  let ans = 0;
+  for (let i = smaller; i <= larger; i++) {
+    ans += i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
