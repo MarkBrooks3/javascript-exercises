f9ebe46f85ffdfe3b86289a6f84ec718419a6c84
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-28 19:41:12 -0500

    finished foundations13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let comp = str.replace(/\W/g, '').toUpperCase();
+  let start = 0;
+  let end = comp.length - 1;
+  console.log(comp);
+  while (start <= end) {
+    if (comp[start] != comp[end]) return false;
+    start++;
+    end--;
+  }
+  return true;
 };
 
 // Do not edit below this line
