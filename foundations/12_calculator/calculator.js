d3d2f8d55515c6e35182ede320ec12c6428e19cb
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-28 19:27:22 -0500

    completed foundations12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, curr) => (sum !== 0 ? sum * curr : curr), 0);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  if (power == 0) return 1;
+  let total = base;
+  for (let i = 1; i < power; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) return 1;
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
