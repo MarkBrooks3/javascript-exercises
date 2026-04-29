a3211c2e90adc7ad9297246f7ff5b419358836a6
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-28 20:10:26 -0500

    finished foundations16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let currentYear = new Date().getFullYear();
+  return arr.reduce((oldest, person) => {
+    let oldDeath = oldest.yearOfDeath ? oldest.yearOfDeath : currentYear;
+    let youngDeath = person.yearOfDeath ? person.yearOfDeath : currentYear;
+    let old = oldDeath - oldest.yearOfBirth;
+    let young = youngDeath - person.yearOfBirth;
+    return old > young ? oldest : person;
+  });
 };
 
 // Do not edit below this line
