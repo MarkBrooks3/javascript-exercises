e227b650e2eb87f3bcce01dbbcbd362a3fb10215
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-28 19:56:04 -0500

    finished foundations14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num == 1) return 1;
+  if (num == 2) return 1;
+  if (num < 0) return 'OOPS';
+  let sum = 0;
+  let first = 1;
+  let second = 1;
 
+  for (let i = 3; i <= num; i++) {
+    console.log(`${i}`);
+    sum = first + second;
+    first = second;
+    second = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
